ebf235e6bd28e73a2a03e0ba6b746ca8ad9d3508
ManideepBangaru <bmd994@gmail.com>
2022-07-28 00:44:20 +0530

    added package.jl

---
+++ 01_packages.jl
@@ -0,0 +1,11 @@
+# packages
+using Statistics
+using BenchmarkTools
+
+# adding packages
+# add BenchmarkTools  ----> do this in pkg mode
+
+# removing packages
+# rm BenchmarkTools ----> do this in pkg mode
+
+# adding a specific version
